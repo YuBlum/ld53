b22ef7bf535b4269a68bd358cf3001a50cd486d6
blum <yuuybonito@gmail.com>
2023-05-01 16:02:44 -0300

    testing3

--- dev/src/game.c
+++ dev/src/game.c
@@ -13,8 +13,13 @@ void
 game_begin(void) {
 	struct v2f start_position = dungeon_generate();
 	player_begin(start_position, backpack_begin(start_position));
+	sound_master_begin();
+	music        = sound_alloc("dungeon");
+	music_source = sound_source_alloc(music, 1);
 	printf("here 1\n");
+	sound_source_play(music_source);
 	printf("here 2\n");
+	sound_source_gain(music_source, 0.5f);
 	printf("here 3\n");
 }
 
@@ -37,5 +42,8 @@ game_draw_ui(void) {
 
 void
 game_end(void) {
+	sound_source_free(music_source);
+	sound_free(music);
+	sound_master_end();
 	dungeon_generator_cleanup();
 }
